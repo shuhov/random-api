19d752ff8cb82080ac11606a5c69e388ceb295c2
shuhov <shuhovivan@yandex.ru>
2017-10-07 18:10:32 +0300

    Change virtualenv activation

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export FLASK_APP=./src/app.py
-source $(pipenv --venv)/bin/activate
-uwsgi --socket 0.0.0.0:5000 --protocol=http --ini random_api.ini --wsgi-file wsgi.py
+source ~/anaconda3/bin/activate random-api-env
+uwsgi --socket 0.0.0.0:5000 --protocol=http --ini random_api.ini --wsgi-file wsgi.py --logto /tmp/uwsgi.log
