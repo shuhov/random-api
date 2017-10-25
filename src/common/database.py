499464300204c60a64090b0e55ca741040544919
shuhov <shuhovivan@yandex.ru>
2017-10-25 23:41:22 +0300

    Add database configuration functions

---
+++ src/common/database.py
@@ -0,0 +1,13 @@
+from flask import g, current_app
+
+from ticket_system.common.pg_connector import Connector
+
+
+def connect_db():
+    return Connector(current_app.config["DB_URI"], schema=current_app.config["DB_SCHEMA"])
+
+
+def get_connection():
+    if not hasattr(g, 'pg_conn'):
+        g.pg_conn = connect_db()
+    return g.pg_conn
