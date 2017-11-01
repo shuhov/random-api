b803a4811dcf6b5f3ac533c9de25f73364d93e5d
shuhov <shuhovivan@yandex.ru>
2017-11-02 00:22:54 +0300

    Add marshalling

--- src/api/resources/network/ipv4_address.py
+++ src/api/resources/network/ipv4_address.py
@@ -1,12 +1,33 @@
-import random
-import socket
-import struct
-
 from flask import jsonify, make_response
 from flask_restful import Resource, reqparse
+from flask_restful_swagger_2 import swagger
+from flask_restful import marshal
+
+from src.common.serializers import IPv4Serializer
+from src.models import IPv4
 
 
 class IPv4Address(Resource):
+    @swagger.doc({
+        'tags': ['IPv4Address'],
+        'description': 'Returns a random IPv4Address',
+        'parameters': [
+            {
+                'name': 'subnet mask',
+                'description': 'logical subdivision of an IP network',
+                'type': 'integer'
+            }
+        ],
+        'responses': {
+            '200': {
+                'description': 'IPv4Address',
+                "resource": {
+                    "name": "ip_address",
+                    "value": "random ipv4 address"
+                }
+            }
+        }
+     })
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('mask', type=int, location='json')
@@ -16,11 +37,5 @@ class IPv4Address(Resource):
         self.name = 'ip_address'
 
     def get(self):
-        ip_address = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
-        data = {
-            'resource': {
-                'name': 'ip_address',
-                'value': ip_address,
-            }
-        }
-        return make_response(jsonify(data=data), 200)
+        return marshal(IPv4(), IPv4Serializer()), 200
+        # return make_response(jsonify(data=data), 200)
