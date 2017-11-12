dba4ffa8f4d06ee071f8b85f54b220c96a92c5a8
shuhov <shuhovivan@yandex.ru>
2017-11-12 22:05:05 +0300

    Remove unnecessary code

--- project/src/api/resources/network.py
+++ project/src/api/resources/network.py
@@ -1,31 +1,13 @@
 from flask_restful import Resource, reqparse
 from flask_restful import marshal
-from project.src.models import IPv4
 
-from project.src.common.serializers import IPv4Serializer
+from project.src.models import IPv4
+from project.src.common.fields import IPv4Fields
+from flask_restful_swagger import swagger
 
 
 class IPv4Address(Resource):
-    docs = {
-        'tags': ['IPv4Address'],
-        'description': 'Returns a random IPv4Address',
-        'parameters': [
-            {
-                'name': 'subnet mask',
-                'description': 'logical subdivision of an IP network',
-                'type': 'integer'
-            }
-        ],
-        'responses': {
-            '200': {
-                'description': 'IPv4Address',
-                "resource": {
-                    "name": "ip_address",
-                    "value": "random ipv4 address"
-                }
-            }
-        }
-     }
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('mask', type=int, location='json')
@@ -35,7 +17,6 @@ class IPv4Address(Resource):
         self.name = 'ip_address'
 
     def get(self):
-        return marshal(IPv4(), IPv4Serializer.resource_fields), 200
-        # return make_response(jsonify(data=data), 200)
+        return marshal(IPv4(), IPv4Fields.resource_fields), 200
 
 
