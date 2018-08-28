871080ebb143c70e943342a7fa5723f214dc9cff
shuhov <shuhovivan@yandex.ru>
2018-08-28 22:07:30 +0300

    Add rbac config holder

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,7 @@
 from flask_restplus import Api
+from flask_rbac import RBAC
+
+from src.api.roles import Role, User
 
 api = Api(
     prefix='/random',
@@ -6,6 +9,21 @@ api = Api(
     version='1.0',
 )
 
+
+class RolesConfig:
+    __instance = None
+
+    def __init__(self):
+        self.rbac = RBAC()
+
+    def init(self, app):
+        self.rbac.init_app(app)
+        return self.rbac
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(RolesConfig, cls).__new__(cls)
+        return cls.__instance
 
 
 
