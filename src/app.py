b5c70c2fb2108fdbd6bb6bcc774b6316206b92a0
shuhov <shuhovivan@yandex.ru>
2018-08-28 22:08:19 +0300

    Implement basic role base model

--- src/app.py
+++ src/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, Response
+from flask_restplus import Namespace, Resource
 
 from src.common.database import get_connection
 import src.common.exceptions as exc
+from src.api import RolesConfig
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('src.config.TestConfig')
+
+    # @app.before_request
+    # def set_current_user():
+    #     user = UserRole(roles=[anonymous])
+    #     g.current_user = user
+
     return app
 
 
@@ -15,10 +23,44 @@ def configure_api():
     from src.api.resources.network.resource import api as ns_network
     from src.api.resources.network.resource import IPv4Address
 
+    configure_rbac(app, api)
+
     api.add_namespace(ns_network, '/network')
     ns_network.add_resource(IPv4Address, '/ipv4_address')
 
     return api
+
+
+def configure_rbac(app, api):
+
+    from src.api.roles import Role, User
+
+    rbac = RolesConfig().init(app)
+
+    anonymous_role = Role('anonymous')
+    anonymous_user = User(roles=[anonymous_role])
+
+    rbac.set_role_model(Role)
+    rbac.set_user_model(User)
+    rbac.set_user_loader(lambda: anonymous_user)
+
+    @app.route('/d')
+    @rbac.deny(roles=['anonymous'], methods=['GET'])
+    def d():
+        return Response('Hello from /d')
+
+    class E(Resource):
+
+        def get(self):
+            return Response('Hello from /e')
+
+    e = Namespace('e', description='e')
+    api.add_namespace(e, '/e')
+    e.add_resource(E, '/e')
+
+    resource = app.view_functions.get('e_e', None)
+    role = rbac._role_model.get_by_name('anonymous')
+    rbac.acl.deny(role, 'GET', resource)
 
 
 def configure_hook(app):
@@ -55,6 +97,6 @@ def error_handler(error):
 
 
 if __name__ == '__main__':
-    print app.url_map
+    print(app.url_map)
     app.run(debug=app.config['DEBUG'])
 
