32f786dc875ccceef0ec6f22a67ed4f68367e29b
shuhov <shuhovivan@yandex.ru>
2019-12-30 23:32:09 +0300

    Skip tests

--- test/task_manager/test_quote.py
+++ test/task_manager/test_quote.py
@@ -3,14 +3,14 @@ import pytest
 from random_api.common.repository.quotes import get_random_quote, request_quote_api
 
 
-@pytest.skip
+@pytest.mark.skip
 def test_request_quote_api():
     response = request_quote_api()
     assert hasattr(response, "json")
     assert [key in response.json() for key in ["content", "author"]]
 
 
-@pytest.skip
+@pytest.mark.skip
 def test_get_random_quote():
     quote = get_random_quote()
     assert isinstance(quote, str)
